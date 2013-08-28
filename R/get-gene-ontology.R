cfcdfa408a4d648ab8805a1ea5f817226382212c
Gil Tomas <gil.tms@gmail.com>
2013-08-28 15:23:37 +0200

    Rewrote getGO: function now avoids mget

--- R/get-gene-ontology.R
+++ R/get-gene-ontology.R
@@ -1,37 +1,28 @@
 getGO <- function (vec,
                    is.symbol = TRUE,
                    path = NULL) {
-    ## require (org.Hs.eg.db)
-    ## reformats GO.db keys
-    myGO2df <- function (go) {
-        dfr <- as.data.frame (Term (go$GOID))
-        dfr <- cbind (go$Evidence, go$Ontology, dfr)
-        colnames (dfr) <- c ("go.evidence", "go.ontology", "term")
-        dfr
-    }
-    
-    tab <- toTable (org.Hs.eg.db::org.Hs.egSYMBOL)
-    
+    ## removes duplicated values
+    vec <- unique (vec)
+    ## defines entrez gene IDs
     if (is.symbol) {
-        vec <- vec[vec %in% tab$symbol]
-        tab <- tab[tab$symbol %in% vec, ]
+        symb2eg.dtb <- data.table (toTable (org.Hs.egSYMBOL2EG))
+        egs <- symb2eg.dtb[symbol %in% vec, gene_id]
     } else {
-        vec <- as.character (vec)
-        vec <- vec[vec %in% tab$gene_id]
-        tab <- tab[tab$gene_id%in% vec, ]
+        egs <- as.character (vec)
     }
-    
-    go.terms <- mget (tab$gene_id, org.Hs.eg.db::org.Hs.egGO, ifnotfound = NA)
-    go.terms <- setNames (go.terms, tab$symbol)
-    go.terms <- go.terms[!is.na (go.terms)]
-    go.lst <- llply (go.terms, function (gene) ldply (gene, myGO2df))
-    go.dfr <- ldply (names (go.lst), function (gene) {
-        cbind (rep (gene, nrow (go.lst[[gene]])), go.lst[[gene]])
-    })
-    colnames (go.dfr)[1:2] <- c ("gene", "go") 
-    
+    ## produces data frame with GO terms associated with entrez gene IDs
+    go.terms <- toTable (org.Hs.egGO[egs])
+    go.terms$Term <- Term (go.terms$go_id)
+    ## adds gene symbols and transforms data frame into data table
+    setkey (symb2eg.dtb, "gene_id")
+    go.terms <- data.table (go.terms, key = "gene_id")
+    dtb <- symb2eg.dtb[go.terms]
+    ## rewrites colnames
+    setnames (dtb, tolower (gsub ("_", ".", names (dtb))))
+    ## saves table on disk if path != NULL
     if (!is.null (path)) {
-        write.csv2 (go.dfr, file = path)
+        write.csv2 (dtb, file = path)
     }
-    data.table (go.dfr)
+    ## returns data table
+    dtb
 }
